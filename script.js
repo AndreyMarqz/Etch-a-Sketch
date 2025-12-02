b1cabe26d2a09c6082c90ad3fcbb04810d8be670
Andrey Marques <andreymmarques6@gmail.com>
2025-12-02 00:25:56 -0300

    Finished the etch-a-sketch project, completed all the logical part of painting the squares with JavaScript and also applied flexbox using CSS.

--- script.js
+++ script.js
@@ -1,23 +1,68 @@
-const container = document.querySelector(".container");
-const gridSize = 16;
+const body = document.querySelector("body");
+const workSection = document.querySelector(".workSection");
+const container = document.querySelector("#container");
+const btn = document.querySelector(".btn");
+const inputBox = document.querySelector(".inputBox");
+const clearBtn = document.querySelector(".clearBtn");
 
-function createGrid() {
-    let input = prompt("How many squares do you want? (from 1 to 100)");
-    let value = parseInt(input); 
+const checkBox = document.createElement("input");
+checkBox.classList.add("checkBox");
 
-    while (isNaN(value) || value < 1 || value > 100) {
-        if (input === null) return;
-        alert("Type a valid number between 1 and 100!");
-        input = prompt("How many squares do you want? (from 1 to 100)");
-        if (input === null) return; 
-        value = parseInt(input);
-    }
-
-    for(let i = 0; i < value; i++){
-        const grid = document.createElement("div");
-        grid.classList.add("squareDiv");
-        container.appendChild(grid);
-    }
+function randomColor() {
+  const h = Math.floor(Math.random() * 360);
+  return `hsl(${h}, 100%, 30%)`;
 }
 
-createGrid();
+function createGrid(size) {
+  container.innerHTML = "";
+  const itemSize = `calc(100% / ${size})`;
+
+  const checkedOrNot = document.querySelector(".checkBox");
+  checkedOrNot.checked = false;
+
+  for (let i = 0; i < size * size; i++) {
+    const squareDiv = document.createElement("div");
+
+    squareDiv.classList.add("squareDiv");
+    squareDiv.style.width = itemSize;
+    squareDiv.style.height = itemSize;
+    squareDiv.style.border = "none";
+
+    squareDiv.addEventListener("mouseover", function (e) {
+      e.target.style.backgroundColor = randomColor();
+
+      if (checkedOrNot.checked) {
+        let currentOpacity = Number(e.target.style.opacity);
+        if (currentOpacity < 1) {
+          e.target.style.opacity = currentOpacity + 0.1;
+        }
+      } else {
+        e.target.style.opacity = 1;
+      }
+    });
+
+    container.appendChild(squareDiv);
+  }
+}
+
+btn.addEventListener("click", () => {
+  let size = inputBox.value;
+  size = parseInt(size);
+
+  if (size && size > 0 && size <= 100) {
+    createGrid(size);
+  } else {
+    alert("Type a number between 1 and 100, please!");
+    inputBox.value = "";
+    inputBox.focus();
+  }
+});
+
+clearBtn.addEventListener("click", () => {
+  const square = document.querySelectorAll(".squareDiv");
+  square.forEach((square) => {
+    square.style.backgroundColor = "white";
+  });
+});
+
+createGrid(16);
